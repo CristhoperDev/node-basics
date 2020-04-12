717eed95d952a991b72e548652c84d00f2524a6b
Cristhoper Alvarez <acristhoper25@gmail.com>
2020-04-12 11:46:21 -0500

    differences between let and var

---
+++ 02-fundamentals/let-var.js
@@ -0,0 +1,15 @@
+// let name = `Wolverine`;
+
+// if (true) {
+//     name = `Magneto`;
+// }
+
+// console.log(name);
+
+var i = 'Hello world';
+
+for (let i = 0; i <= 5; i++) {
+    console.log(`i: ${i}`);
+}
+
+console.log(i);
