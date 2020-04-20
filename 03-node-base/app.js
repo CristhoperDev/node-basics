b4ce83cc7f39a6a5d2bdf1f0a384772ca2cd2a90
Cristhoper Alvarez <acristhoper25@gmail.com>
2020-04-20 12:00:48 -0500

    read argv with yargs

--- 03-node-base/app.js
+++ 03-node-base/app.js
@@ -9,18 +9,36 @@ const argv = require('yargs')
             default: 10
         }
     })
+    .command('create', 'Create file with multiply table', {
+        base: {
+            demand: true,
+            alias: 'b'
+        },
+        limit: {
+            alias: 'l',
+            default: 10
+        }
+    })
     .help()
     .argv;
-const { createFile } = require('./multiply/multiply');
 
-let argv2 = process.argv;
-console.log(argv.base);
-console.log(argv.limit);
-//let parameter = argv[2];
-//let base = parameter.split('=')[1];
+const { createFile, listTable } = require('./multiply/multiply');
 
-/*
-createFile(base)
-    .then(file => console.log(`File created: ${file}`))
-    .catch(err => console.log(err));
-*/
+let command = argv._[0];
+switch (command) {
+    case 'list':
+        listTable(argv.base, argv.limit);
+        break;
+    case 'create':
+        createFile(argv.base, argv.limit)
+            .then(file => console.log(`File created: ${file}`))
+            .catch(err => console.log(err));
+
+        break;
+    default:
+        console.log('Unrecognized command')
+}
+//console.log(argv);
+
+//console.log(argv.base);
+
--- 03-node-base/multiply/multiply.js
+++ 03-node-base/multiply/multiply.js
@@ -1,26 +1,33 @@
 //required
 const fs = require('fs');
 
-let createFile = (base) => {
+let listTable = (base, limit = 10) => {
+    for (let i = 1; i <= limit; i++) {
+        console.log(`${base} * ${i} = ${base * i}`);
+    }
+}
+
+let createFile = (base, limit = 10) => {
     return new Promise(((resolve, reject) => {
-        if (!Number(base)){
+        if (!Number(base)) {
             reject(`The entered value ${base} is not a number`);
             return;
         }
         let data = '';
 
-        for (let i = 1; i <= 10; i++) {
+        for (let i = 1; i <= limit; i++) {
             data += `${base} * ${i} = ${base * i}\n`;
         }
 
-        fs.writeFile(`files/table-${base}.txt`, data, (err) => {
+        fs.writeFile(`files/table-${base}-to-${limit}.txt`, data, (err) => {
             if (err) reject(err);
-            resolve(`table-${base}.txt`);
+            resolve(`table-${base}-to-${limit}.txt`);
         });
 
     }));
 }
 
 module.exports = {
-    createFile
+    createFile,
+    listTable
 }
