349af079f2f3ad03cb1cc189d2692db2bfce21f3
Cristhoper Alvarez <acristhoper25@gmail.com>
2020-04-20 11:33:14 -0500

    yargs arguments

--- 03-node-base/app.js
+++ 03-node-base/app.js
@@ -1,9 +1,26 @@
+const argv = require('yargs')
+    .command('list', 'Print in console multiply table', {
+        base: {
+            demand: true,
+            alias: 'b'
+        },
+        limit: {
+            alias: 'l',
+            default: 10
+        }
+    })
+    .help()
+    .argv;
 const { createFile } = require('./multiply/multiply');
 
-let argv = process.argv;
-let parameter = argv[2];
-let base = parameter.split('=')[1];
+let argv2 = process.argv;
+console.log(argv.base);
+console.log(argv.limit);
+//let parameter = argv[2];
+//let base = parameter.split('=')[1];
 
+/*
 createFile(base)
     .then(file => console.log(`File created: ${file}`))
     .catch(err => console.log(err));
+*/
