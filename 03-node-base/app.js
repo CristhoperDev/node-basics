dc89ff14166a280b57acf8ecaecda980878d15e8
Cristhoper Alvarez <acristhoper25@gmail.com>
2020-04-20 11:00:47 -0500

    sending parameter in console

--- 03-node-base/app.js
+++ 03-node-base/app.js
@@ -1,6 +1,8 @@
 const { createFile } = require('./multiply/multiply');
 
-let base = '5';
+let argv = process.argv;
+let parameter = argv[2];
+let base = parameter.split('=')[1];
 
 createFile(base)
     .then(file => console.log(`File created: ${file}`))
