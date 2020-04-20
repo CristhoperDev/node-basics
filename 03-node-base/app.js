75296c69a1f14356af94819acb05caa48c16a366
Cristhoper Alvarez <acristhoper25@gmail.com>
2020-04-20 10:31:15 -0500

    required package

--- 03-node-base/app.js
+++ 03-node-base/app.js
@@ -1,6 +1,14 @@
-let base = 3;
+//required
+const fs = require('fs');
 
+let base = 2;
+
+let data = '';
 for (let i = 1; i<=10; i++) {
-    console.log(`${base} * ${i} = ${base * i}`);
+    data += `${base} * ${i} = ${base * i}\n`;
 }
 
+fs.writeFile(`files/table-${base}.txt`, data, (err) => {
+    if (err) throw err;
+    console.log(`The file table-${base}.txt has been saved!`);
+});
