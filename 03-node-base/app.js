db65bb1934eb1cda3f1b1d7786bdbf541d4b796e
Cristhoper Alvarez <acristhoper25@gmail.com>
2020-04-20 12:13:53 -0500

    colors in console

--- 03-node-base/app.js
+++ 03-node-base/app.js
@@ -1,4 +1,5 @@
 const argv = require('./config/yargs').argv;
+const colors = require('colors/safe');
 const { createFile, listTable } = require('./multiply/multiply');
 
 let command = argv._[0];
@@ -10,7 +11,7 @@ switch (command) {
 
     case 'create':
         createFile(argv.base, argv.limit)
-            .then(file => console.log(`File created: ${file}`))
+            .then(file => console.log(`File created:`, colors.green(file)))
             .catch(err => console.log(err));
 
         break;
--- 03-node-base/multiply/multiply.js
+++ 03-node-base/multiply/multiply.js
@@ -1,7 +1,11 @@
 //required
 const fs = require('fs');
+const colors = require('colors');
 
 let listTable = (base, limit = 10) => {
+    console.log('=========================='.green);
+    console.log(`========table of ${base}========`.green);
+    console.log('=========================='.green);
     for (let i = 1; i <= limit; i++) {
         console.log(`${base} * ${i} = ${base * i}`);
     }
